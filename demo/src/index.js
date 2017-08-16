ec4066d02148dd0c6c3c5d64cfa108234a9706ea
jaywcjlove <398188662@qq.com>
2017-08-17 03:02:08 +0800

    Update demo.

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
 import Example from '../../src'
 
@@ -26,15 +26,27 @@ class Demo extends Component {
   }
   render() {
     return (
-      <Example 
-        keyName="shift+a,alt+s" 
+      <Example
+        keyName="shift+a,alt+s"
         onKeyDown={this.onKeyDown.bind(this)}
         onKeyUp={this.onKeyUp.bind(this)}
       >
-        <div style={{ padding: "20px" }} dangerouslySetInnerHTML={{__html:this.state.output}} />
-      </Example>
+        <div style={{ padding: "10px 20px 0px" }}>
+          <h1 style={{ padding: "0px 0px 0px" }}>react-hotkeys</h1>
+          <div>
+            <a href="https://travis-ci.org/jaywcjlove/react-hotkeys"><img src="https://travis-ci.org/jaywcjlove/react-hotkeys.svg?branch=master" /></a>&nbsp;
+            <a href="https://www.npmjs.org/package/react-hot-keys"><img src="https://img.shields.io/npm/v/react-hot-keys.svg" /></a>&nbsp;
+            <a href="https://coveralls.io/github/jaywcjlove/react-hotkeys"><img src="https://img.shields.io/coveralls/jaywcjlove/react-hotkeys/master.svg" /></a>
+          </div>
+          <p style={{ padding: "10px 0px 0px" }}>
+            React component to listen to keydown and keyup keyboard events, defining and  dispatching keyboard shortcuts.
+            Uses a fork of <b><a href="https://github.com/jaywcjlove/hotkeys">hotkeys.js</a></b> for keydown detection of special characters. You give it a keymap of shortcuts & it bind it to the mousetrap singleton. The, it'll unbind it when the component unmounts.
+          </p>
+          <div style={{ padding: "20px 0 20px" }} dangerouslySetInnerHTML={{ __html: this.state.output }} />
+        </div>
+      </Example >
     )
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector('#demo'))
