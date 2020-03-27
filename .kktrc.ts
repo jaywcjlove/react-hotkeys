239a860195a073290e3293465c29d0c4979544b1
jaywcjlove <398188662@qq.com>
2020-03-27 10:00:12 +0800

    fix: Fix kkt config.

--- .kktrc.ts
+++ .kktrc.ts
@@ -1,24 +1,35 @@
 import path from 'path';
+import webpack from 'webpack';
+import { OptionConf } from 'kkt/lib/config/webpack.config';
 
 export const loaderOneOf = [
   require.resolve('@kkt/loader-less')
 ];
 
+interface Options extends OptionConf {
+  yargsArgs: OptionConf['yargsArgs'] & {
+    bundle: boolean;
+    mini: boolean;
+  };
+}
+
 /**
  * Bundles a minified and unminified version of UIW including
  * all it's immediate dependencies (excluding React, ReactDOM, etc)
  */
-export default (conf, options) => {
+export default (conf: webpack.Configuration, options: Options) => {
   if (options.yargsArgs && options.yargsArgs.bundle) {
     const isMini = options.yargsArgs && options.yargsArgs.mini;
     conf.devtool = false;
-    const regexp = /(HtmlWebpackPlugin|InlineChunkHtmlPlugin|InterpolateHtmlPlugin|MiniCssExtractPlugin|ManifestPlugin|IgnorePlugin|GenerateSW)/;
-    conf.plugins = conf.plugins.map((item) => {
-      if (item.constructor && item.constructor.name && regexp.test(item.constructor.name)) {
-        return null;
-      }
-      return item;
-    }).filter(Boolean);
+    if (conf.plugins) {
+      const regexp = /(HtmlWebpackPlugin|InlineChunkHtmlPlugin|InterpolateHtmlPlugin|MiniCssExtractPlugin|ManifestPlugin|IgnorePlugin|GenerateSW)/;
+      conf.plugins = conf.plugins.map((item) => {
+        if (item.constructor && item.constructor.name && regexp.test(item.constructor.name)) {
+          return null;
+        }
+        return item;
+      }).filter(Boolean) as webpack.Plugin[];
+    }
     conf.entry = './src/index.tsx';
     conf.output = {
       path: path.join(process.cwd(), 'dist'),
@@ -50,7 +61,7 @@ export default (conf, options) => {
     if (options.yargsArgs && options.yargsArgs.mini) {
       conf.output.filename = 'react-hotkeys.min.js';
     } else {
-      conf.optimization.minimize = false;
+      conf.optimization!.minimize = false;
     }
     conf.optimization = {
       minimize: options.isEnvProduction,
