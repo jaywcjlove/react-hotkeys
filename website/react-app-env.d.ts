3456340cbec8a734fb2ff75fe7269df7a7d8ee1d
jaywcjlove <398188662@qq.com>
2021-09-04 02:10:28 +0800

    chore(deps): update dependency tsbb to v3

--- website/react-app-env.d.ts
+++ website/react-app-env.d.ts
@@ -1,5 +1,8 @@
+/// <reference types="react-scripts" />
 
-declare module '*.md' {
-  const src: string;
-  export default src;
+declare module '*.less' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
 }
+
+declare module '*.md';
